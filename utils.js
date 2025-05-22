068ad08785e90204a67d5854c1d8ff59bf0f8621
Kasper <106410740+kasperwtrcolor@users.noreply.github.com>
2025-05-22 17:15:47 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,7 +1,3 @@
-export function extractPrompt(tweetText) {
-  return tweetText.replace(/@bot_wassy/gi, '').trim();
-}
-
 export async function uploadImageToTwitter(client, buffer) {
   return await client.v1.uploadMedia(Buffer.from(buffer), {
     mimeType: 'image/png',
